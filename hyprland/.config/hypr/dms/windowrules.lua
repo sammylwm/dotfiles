8f8ac9bf8aba32c55554381203149eac40275c6a
sammy <lednevdosa@gmail.com>
2026-07-13 17:31:28 +0300

    localsend

--- hyprland/.config/hypr/dms/windowrules.lua
+++ hyprland/.config/hypr/dms/windowrules.lua
@@ -4,3 +4,4 @@
 hl.workspace_rule({ workspace = "special:happ", on_created_empty = "happ" })
 hl.workspace_rule({ workspace = "special:tg", on_created_empty = "materialgram" })
 hl.workspace_rule({ workspace = "special:music", on_created_empty = "yandex-music" })
+hl.workspace_rule({ workspace = "special:localsend", on_created_empty = "localsend" })
