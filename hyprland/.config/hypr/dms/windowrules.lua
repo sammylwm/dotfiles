9542f082e609c2fc33e831de8bcf1daee731c023
sammy <lednevdosa@gmail.com>
2026-07-14 13:41:15 +0300

    2026-07-14

--- hyprland/.config/hypr/dms/windowrules.lua
+++ hyprland/.config/hypr/dms/windowrules.lua
@@ -5,3 +5,20 @@ hl.workspace_rule({ workspace = "special:happ", on_created_empty = "happ" })
 hl.workspace_rule({ workspace = "special:tg", on_created_empty = "materialgram" })
 hl.workspace_rule({ workspace = "special:music", on_created_empty = "yandex-music" })
 hl.workspace_rule({ workspace = "special:localsend", on_created_empty = "localsend" })
+
+hl.window_rule({
+  name = "kando",
+  match = {
+      class = "menu.kando.Kando",
+      title = "Kando Menu"
+  },
+  no_blur = true,
+  opaque = true,
+  move = {0, 0},
+  rounding = 0,
+  size = { "100%", "100%" },
+  border_size = 0,
+  no_anim = true,
+  float = true,
+  pin = true
+})
---
+++ nvim/.config/nvim/lua/globals.lua
@@ -0,0 +1,12 @@
+---@meta
+
+---@class Hyprland
+hl = {}
+
+function hl.config(...) end
+function hl.on(...) end
+function hl.exec_cmd(...) end
+function hl.animation(...) end
+function hl.window_rule(...) end
+function hl.layer_rule(...) end
+function hl.env(...) end
