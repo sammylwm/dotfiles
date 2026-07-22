19fd84e05560b3de5d3b38ffb5857a800b3a5b8c
sammylwm <lednevdosa@gmail.com>
2026-07-22 20:46:25 +0300

    2026-07-22

--- lazynvim/.config/nvim/lua/config/keymaps.lua
+++ lazynvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,27 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local Snacks = require("snacks")
+
+-- <leader>e -> текущая директория
+vim.keymap.set("n", "<leader>e", function()
+  Snacks.explorer({
+    cwd = vim.fn.getcwd(),
+  })
+end, { desc = "Explorer (cwd)" })
+
+-- <leader>E -> корень проекта
+vim.keymap.set("n", "<leader>E", function()
+  Snacks.explorer({
+    cwd = require("lazyvim.util").root.get(),
+  })
+end, { desc = "Explorer (root)" })
+
+local bufferline = require("bufferline")
+
+for i = 1, 9 do
+  vim.keymap.set("n", "<A-" .. i .. ">", function()
+    bufferline.go_to(i, true)
+  end, { desc = "Go to buffer " .. i })
+end
+
+vim.keymap.set("n", "<C-w>", function()
+  Snacks.bufdelete()
+end, { desc = "Close current file" })
