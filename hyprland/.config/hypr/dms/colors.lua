9f14d1d69fb8e60db2fd6d107f8d327a0acb1378
sammylwm <lednevdosa@gmail.com>
2026-07-23 14:14:31 +0300

    2026-07-23

--- hyprland/.config/hypr/dms/colors.lua
+++ hyprland/.config/hypr/dms/colors.lua
@@ -4,23 +4,23 @@
 hl.config({
 	general = {
 		col = {
-			active_border = "rgb(ffb0cf)",
-			inactive_border = "rgb(9d8c91)",
+			active_border = "rgb(85d6bf)",
+			inactive_border = "rgb(89938f)",
 		},
 	},
 	group = {
 		col = {
-			border_active = "rgb(ffb0cf)",
-			border_inactive = "rgb(9d8c91)",
+			border_active = "rgb(85d6bf)",
+			border_inactive = "rgb(89938f)",
 			border_locked_active = "rgb(ffb4ab)",
-			border_locked_inactive = "rgb(9d8c91)",
+			border_locked_inactive = "rgb(89938f)",
 		},
 		groupbar = {
 			col = {
-				active = "rgb(ffb0cf)",
-				inactive = "rgb(9d8c91)",
+				active = "rgb(85d6bf)",
+				inactive = "rgb(89938f)",
 				locked_active = "rgb(ffb4ab)",
-				locked_inactive = "rgb(9d8c91)",
+				locked_inactive = "rgb(89938f)",
 			},
 		},
 	},
