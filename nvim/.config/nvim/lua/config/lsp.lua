7043477bce69c7b831207b218ca254a3861c7989
sammy <lednevdosa@gmail.com>
2026-08-03 15:18:30 +0300

    2026-08-03

---
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -0,0 +1,9 @@
+vim.lsp.enable("pyright")
+
+vim.lsp.config("qml-language-server", {
+  cmd = { "qml-language-server" },
+  filetypes = { "qml" },
+  root_markers = { { "qmldir", "shell.qml" }, ".git" },
+})
+
+vim.lsp.enable("qml-language-server")
