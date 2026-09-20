8d4758d620387d41935613caa810dfedc88705b0
sammy <lednevdosa@gmail.com>
2026-09-20 19:54:51 +0300

    2026-09-20

--- sammy-nvim/.config/nvim/lua/configs/options.lua
+++ sammy-nvim/.config/nvim/lua/configs/options.lua
@@ -34,14 +34,4 @@ vim.opt.langmap = vim.fn.join({
 	escape(ru) .. ";" .. escape(en),
 }, ",")
 
--- Подключение clangd через встроенный LSP
-vim.lsp.config.clangd = {
-  cmd = { "clangd" },
-  filetypes = { "c", "cpp", "objc", "objcpp" },
-  root_dir = function(fname)
-    local util = require('lspconfig.util')
-    return util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(fname) or util.path.dirname(fname)
-  end,
-}
-vim.lsp.enable('clangd')
 
